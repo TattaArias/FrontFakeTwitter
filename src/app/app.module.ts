9f332e1d2058f0f36f70f233081f9b06466a063d
Tatiana Cañón <55030034+TattaArias@users.noreply.github.com>
2020-04-06 12:10:09 -0500

    Se actualiza imports necesarios para los estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TweetComponent } from './tweet/tweet.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListaTweetsComponent } from './lista-tweets/lista-tweets.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TweetComponent
+    TweetComponent,
+    ListaTweetsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
